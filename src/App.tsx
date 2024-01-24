d7877d0585d8e8872c3739b9e006ef1fbece4ebf
Corey Casmedes <ccasmedes3@gmail.com>
2024-01-24 00:10:05 +0000

    removed resume link

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,11 @@ const App: React.FC = () => {
             <Link to="/coreycasmedes-ui">Home</Link>
           </li>
           <li className='px-2 py-1 font-assistant-light  ml-4 rounded-md hover:bg-gray-300'>
-            <Link to="/" onClick={() => {window.open(require('./CoreyCasmedesFall2022.pdf'))}}>Resume</Link>
+            <Link 
+              to="/"
+            >
+              Resume
+            </Link>
           </li>
           <li className='px-2 py-1 font-assistant-light  ml-4 rounded-md hover:bg-gray-300'>
             <Link to="/projects">Projects</Link>
@@ -58,25 +62,25 @@ const App: React.FC = () => {
           
           <div className='flex items-center pl-0 lg:pl-8'>
               <a href='https://www.instagram.com/coreycasmedes/' className='px-2'>
-                  <img src={InstagramIcon} className='rounded-full w-8 lg:w-9 bg-white'></img>
+                  <img src={InstagramIcon} className='rounded-full w-8 lg:w-9 border-white border bg-white'></img>
               </a>
           </div>
 
           <div className='flex items-center pl-0'>
               <a href='https://www.instagram.com/coreycasmedes/' className='px-2'>
-                  <img src={GithubIcon} className='rounded-full w-8 lg:w-9 bg-white'></img>
+                  <img src={GithubIcon} className='rounded-full w-8 lg:w-9 border-white border bg-white'></img>
               </a>
           </div>
 
           <div className='flex items-center pl-0 fill-white'>
               <a href='https://www.instagram.com/coreycasmedes/' className='px-2'>
-                  <img src={LinkedInIcon} className='rounded-full w-8 lg:w-9 bg-white'></img>
+                  <img src={LinkedInIcon} className='rounded-full w-8 lg:w-9 border-white border bg-white'></img>
               </a>
           </div>
 
           <div className='flex items-center pl-0 '>
               <a href='https://www.instagram.com/coreycasmedes/' className='px-2'>
-                  <img src={EmailIcon} className='rounded-full w-8 lg:w-9'></img>
+                  <img src={EmailIcon} className='rounded-full w-8 lg:w-9 border-white border'></img>
               </a>
           </div>
       </div>
